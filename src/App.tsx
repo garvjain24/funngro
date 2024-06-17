15f29595230f7d3f3ef4d3103312e6b2937d374f
garvjain24 <garvjain2410@gmail.com>
2024-06-17 18:33:45 +0530

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import TeenPage from './TeenPage';
@@ -7,6 +8,17 @@ import CompanyPage from './components/CompanyPage';
 
 
 const App: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const path = params.get('path');
+    if (path) {
+      navigate(path.replace('/funngro', ''));
+    }
+  }, [navigate]);
+
   return (
     <Router>
       <div className="App">
